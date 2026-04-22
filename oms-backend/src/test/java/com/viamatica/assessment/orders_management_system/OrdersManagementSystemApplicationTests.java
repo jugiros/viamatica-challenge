ad9ae57148aa3dafd234bdfe458fe12e02aab8fa
Juan Molina <jmolina@initiumsoft.com>
2026-04-21 22:42:36 -0500

    feat: init projects integrations

---
+++ oms-backend/src/test/java/com/viamatica/assessment/orders_management_system/OrdersManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viamatica.assessment.orders_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
